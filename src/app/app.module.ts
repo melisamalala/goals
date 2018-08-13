3eff96921a3e9561c75a13027cb57660c08b4e76
melisamalala <melissamalala@gmail.com>
2018-08-13 13:03:13 +0300

    Added quote HTTP and quote class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {AlertsService} from './alert-service/alerts.service';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
@@ -8,6 +10,10 @@ import { GoalDetailsComponent } from './goal-details/goal-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+
+
 
 @NgModule({
   declarations: [
@@ -20,9 +26,12 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(), // normal progress bar
+   NgProgressHttpModule // progress bar to load http requests
   ],
-  providers: [],
+  providers: [AlertsService], // Add service to providers
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -2,17 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import {Goal} from '../goal';
 import {Goals} from '../goals';
 import {GoalService} from '../goals/goal.service';
+import {AlertsService} from '../alert-service/alerts.service';
+import {HttpClient} from '@angular/common/http';
+import {QuoteRequestService} from '../quote-http/quote-request.service';
+
+import {Quote} from '../quote-class/quote';
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  providers: [ GoalService], // add the providers to the component
+  providers: [ GoalService, QuoteRequestService], // add the providers to the component
   styleUrls: ['./goal.component.css']
 })
 
 export class GoalComponent implements OnInit {
+   quote: Quote;
    goals = Goals;
+
    goals: Goal[];
+   alertService: AlertsService;
     goals = [
         new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son', new Date(2018, 3, 14) ),
         new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot', new Date(2018, 6, 9) ),
@@ -36,6 +44,7 @@ export class GoalComponent implements OnInit {
 
             if (toDelete) {
                 this.goals.splice(index, 1);
+                this.alertService.alertMe("Goal has been deleted")
             }
         }
     }
@@ -44,10 +53,27 @@ export class GoalComponent implements OnInit {
         this.goals[index].showDescription = !this.goals[index].showDescription;
     }
 
-    goals:Goal[];
-      constructor(goalService:GoalService) {
-      this.goals = goalService.getGoals()
+
+      constructor(goalService:GoalService, alertService: AlertsService, private http: HttpClient, private quoteService: QuoteRequestService){
+      this.goals = goalService.getGoals();
+      this.alertService = alertService; // make the service available to the class
        }
   ngOnInit() {
+
+    this.quoteService.quoteRequest();
+       this.quote = this.quoteService.quote;
+    interface ApiResponse {
+        quote: string;
+        author: string;
+    }
+
+    this.http.get<ApiResponse>( 'https://talaikis.com/api/quotes/random/').subscribe(data => {
+       this.quote = new Quote(data.quote, data.author);
+     }, err => {
+             this.quote = new Quote("Never, never, never give up.","winston churchill")
+             console.log("Error occured ");
+
+     });
+        // Successful API request.
   }
 }
